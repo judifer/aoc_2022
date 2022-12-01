685578727048ea948654615c33f406e540a521ab
judifer <45107199+judifer@users.noreply.github.com>
2022-12-01 12:31:43 +0100

    Update day01.py

--- day01.py
+++ day01.py
@@ -1,16 +1,4 @@
 with open("day01.txt") as f:
-    inp = [x for x in f.read().split("\n\n")]
-
-cals = list()
-
-for i in inp:
-    cnt = 0
-    sing = [int(x) for x in i.split("\n")]
-    for j in sing:
-        cnt += j
-    cals.append(cnt)
-
-cals.sort()
-
-print("Part one:", cals[-1])
-print("Part two:", sum(cals[-3:]))
+    inp = sorted(sum(int(y) for y in x.split("\n")) for x in f.read().split("\n\n"))
+print("Part one:", inp[-1])
+print("Part two:", sum(inp[-3:]))
