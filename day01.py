e15bae5daa22bafc41e8ce115070a668af33d0bb
judifer <45107199+judifer@users.noreply.github.com>
2022-12-01 16:52:42 +0100

    Update day01.py

--- day01.py
+++ day01.py
@@ -1,4 +1,3 @@
 with open("day01.txt") as f:
     inp = sorted(sum(int(y) for y in x.split("\n")) for x in f.read().split("\n\n"))
-print("Part one:", inp[-1])
-print("Part two:", sum(inp[-3:]))
+print("Part one:", inp[-1], "| Part two:", sum(inp[-3:]))
