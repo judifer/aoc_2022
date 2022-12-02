0c56ddfe538e0f853ff6a1ab9e948d2677dbbaf3
judifer <45107199+judifer@users.noreply.github.com>
2022-12-02 06:51:20 +0100

    Create day02.py

---
+++ day02.py
@@ -0,0 +1,5 @@
+with open("day02.txt") as f:
+    game = [x.strip() for x in f.readlines()]
+part_one = {"A X": 4, "A Y": 8, "A Z": 3, "B X": 1, "B Y": 5, "B Z": 9, "C X": 7, "C Y": 2, "C Z": 6}
+part_two = {"A X": 3, "A Y": 4, "A Z": 8, "B X": 1, "B Y": 5, "B Z": 9, "C X": 2, "C Y": 6, "C Z": 7}
+print(f"Part 1: {sum(part_one[i] for i in game)} | Part 2: {sum(part_two[i] for i in game)}")
