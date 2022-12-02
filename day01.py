f57fa4535df86ccad7b47f7b8c56b8e3f052cfec
judifer <45107199+judifer@users.noreply.github.com>
2022-12-02 10:55:29 +0100

    shortened day 1 + 2

--- day01.py
+++ day01.py
@@ -1,3 +1,2 @@
-with open("day01.txt") as f:
-    inp = sorted(sum(int(y) for y in x.split("\n")) for x in f.read().split("\n\n"))
+inp = sorted(sum(int(y) for y in x.split("\n")) for x in open("day01.txt").read().split("\n\n"))
 print("Part one:", inp[-1], "| Part two:", sum(inp[-3:]))
