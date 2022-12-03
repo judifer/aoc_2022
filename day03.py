0adf4b28911fec0c1deb71b91e6a6c0d238ad860
judifer <45107199+judifer@users.noreply.github.com>
2022-12-03 07:28:41 +0100

    Update day03.py

--- day03.py
+++ day03.py
@@ -1,11 +1,11 @@
 sacks = [x.strip() for x in open("day03.txt").readlines()]
 
 cnt = 0
-
 for sack in sacks:
     first = set(sack[:(len(sack) // 2)])
     second = set(sack[(len(sack) // 2):])
-    letter = list(first & second)[0]
+    cmp = first & second
+    letter = cmp.pop()
     if letter.islower():
         cnt += ord(letter) - 96
     else:
@@ -16,7 +16,8 @@ print("Part 1:", cnt)
 cnt = 0
 for i in range(0, len(sacks), 3):
     a, b, c = sacks[i:i + 3]
-    letter = list(set(a) & set(b) & set(c))[0]
+    cmp = set(a) & set(b) & set(c)
+    letter = cmp.pop()
     if letter.islower():
         cnt += ord(letter) - 96
     else:
