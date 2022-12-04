7ab9b5b915b4d9f6a5e4a47c5830ca228e73bb76
judifer <45107199+judifer@users.noreply.github.com>
2022-12-04 06:43:00 +0100

    Update day04.py

--- day04.py
+++ day04.py
@@ -6,16 +6,8 @@ for section in sections:
     first, second = section[0].split("-")
     third, fourth = section[1].split("-")
     first, second, third, fourth = int(first), int(second), int(third), int(fourth)
-    if first == second:
-        a = set()
-        a.add(first)
-    else:
-        a = set(range(first, second + 1))
-    if third == fourth:
-        b = set()
-        b.add(third)
-    else:
-        b = set(range(third, fourth + 1))
+    a = set(range(first, second + 1))
+    b = set(range(third, fourth + 1))
     if a.issubset(b) or b.issubset(a):
         cnt_1 += 1
     if len(a.intersection(b)) > 0:
