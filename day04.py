ae754fdf91304fd7275f26335df4d0fecc2c6e38
judifer <45107199+judifer@users.noreply.github.com>
2022-12-04 06:56:13 +0100

    Update day04.py

--- day04.py
+++ day04.py
@@ -1,11 +1,10 @@
-sections = [x.strip().split(",") for x in open("day04.txt").readlines()]
+sections = [[y.split("-") for y in x.strip().split(",")] for x in open("day04.txt").readlines()]
 cnt_1, cnt_2 = 0, 0
 
 for section in sections:
-    first, second = section[0].split("-")
-    third, fourth = section[1].split("-")
-    a = set(range(int(first), int(second) + 1))
-    b = set(range(int(third), int(fourth) + 1))
+    first, second, third, fourth = int(section[0][0]), int(section[0][1]), int(section[1][0]), int(section[1][1])
+    a = set(range(first, second + 1))
+    b = set(range(third, fourth + 1))
     if a.issubset(b) or b.issubset(a):
         cnt_1 += 1
     if len(a.intersection(b)) > 0:
