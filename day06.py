994bbf4d41a78adbfdb618c12630ccf41d4f1c47
judifer <45107199+judifer@users.noreply.github.com>
2022-12-06 08:59:20 +0100

    Update day06.py

--- day06.py
+++ day06.py
@@ -1,18 +1,12 @@
 inp = open("day06.txt").read()
 
-for idx, signal in enumerate(inp):
-    marker = set()
-    message = set()
-    for i in range(4):
-        marker.add(inp[idx + i])
-    if len(marker) == 4:
-        break
+def finder(inp, nums):
+    for idx, signal in enumerate(inp):
+        marker = set()
+        message = set()
+        for i in range(nums):
+            marker.add(inp[idx + i])
+        if len(marker) == nums:
+            return idx + nums
 
-for idx_2, signal in enumerate(inp):
-    marker = set()
-    for i in range(14):
-        marker.add(inp[idx_2 + i])
-    if len(marker) == 14:
-        break
-
-print(f"Part one: {idx + 4} | Part two: {idx_2 + 14}")
+print(f"Part one: {finder(inp, 4)} | Part two: {finder(inp, 14)}")
