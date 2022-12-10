c4b2a84c8325e11ccfe7cff04c037691cc816378
judifer <45107199+judifer@users.noreply.github.com>
2022-12-10 08:20:34 +0100

    Create day10.py

---
+++ day10.py
@@ -0,0 +1,33 @@
+inp = [x.strip() for x in open("day10.txt").readlines()]
+
+x = 1
+cnt = 0
+vals = [20, 60, 100, 140, 180, 220]
+imp = 0
+grid = [["."] * 40 for _ in range(6)]
+draw = 0
+
+for line in inp:
+    if line == "noop":
+        if abs(x - cnt % 40) <= 1:
+                grid[draw][cnt % 40] = "#"
+        cnt += 1
+        if cnt in vals:
+            imp += cnt * x
+        if cnt % 40 == 0:
+            draw += 1
+    else:
+        _, a = line.split()
+        for i in range(2):
+            if abs(x - cnt % 40) <= 1:
+                grid[draw][cnt % 40] = "#"
+            cnt += 1
+            if cnt in vals:
+                imp += cnt * x
+            if cnt % 40 == 0:
+                draw += 1
+        x += int(a)
+
+print("Part one:", imp)
+for i in grid:
+    print("".join(i))
